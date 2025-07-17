e38c7c2e2332fe7111eb22438d69bb1ec77a81b5
leticiakremer <leticiakremer@users.noreply.github.com>
2025-07-17 05:40:24 +0000

    deploy: 35d0dd1bedcf8064eded7b4aab05024c4c11ac5e

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -20,7 +20,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "da78491f2a0bfe9f80bc5a8efc79bfe8
 "assets/NOTICES": "924914291abdc62fedde1c44d874ae83",
 "version.json": "1759b97f3054f230c2a17c634defaa60",
 "manifest.json": "3fd04be254d0ed92ecf3f2f2b1804c2f",
-"flutter_bootstrap.js": "4ab40eb404de8238c851590f715ef96a",
+"flutter_bootstrap.js": "38c453d6dcf734a9e06582dd3f94df01",
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "canvaskit/canvaskit.wasm": "7a3f4ae7d65fc1de6a6e7ddd3224bc93",
 "canvaskit/skwasm.js.symbols": "e72c79950c8a8483d826a7f0560573a1",
