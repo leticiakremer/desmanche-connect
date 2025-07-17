90818710c6481d000cb2b1351f49c8ecb5f065e5
leticiakremer <leticiakremer@users.noreply.github.com>
2025-07-17 04:49:36 +0000

    deploy: 773a2aa9fe594e0937c7b26b80bb1b6494ff96da

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"39d6d6e699e51b2874210e14cddf1a22fb9524
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1057632648"
+    serviceWorkerVersion: "4189115298"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -20,7 +20,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "da78491f2a0bfe9f80bc5a8efc79bfe8
 "assets/NOTICES": "924914291abdc62fedde1c44d874ae83",
 "version.json": "1759b97f3054f230c2a17c634defaa60",
 "manifest.json": "3fd04be254d0ed92ecf3f2f2b1804c2f",
-"flutter_bootstrap.js": "c63982947ad1c4c3e4f68ac522da2cdd",
+"flutter_bootstrap.js": "f837f65e90759d375fcb62c79a38e8ae",
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "canvaskit/canvaskit.wasm": "7a3f4ae7d65fc1de6a6e7ddd3224bc93",
 "canvaskit/skwasm.js.symbols": "e72c79950c8a8483d826a7f0560573a1",
